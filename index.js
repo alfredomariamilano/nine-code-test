ae71080383f4d239b5ad3899ee7373d2a74aaeb0
alfredomariamilano <alfmarmil@gmail.com>
2018-02-07 07:13:28 +1100

    Added some comments

--- index.js
+++ index.js
@@ -14,19 +14,16 @@ app.use(morgan('dev'));
 app.use(bodyParser.json({type: 'application/*'}));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Error handler
-app.use((err, req, res, next) => {
-  if (err && err.statusCode === 400 && err.type === 'entity.parse.failed') {
-    return res.status(400).json({
-      error: 'Could not decode request: JSON parsing failed'
-    });
-  }
-  next();
-});
-
-// Allows cross-origin requests from the specified url
+// Allows cross-origin requests from the specified urls
 const corsOptions = {
-  origin: 'https://codingchallenge.nine.com.au/',
+  origin: (origin, callback) => {
+    const nineRegex = /^(https?:\/\/(?:.+\.)?nine\.com.au(?::\d{1,5})?)$/;
+    if (nineRegex.test(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
@@ -34,6 +31,20 @@ app.use(cors(corsOptions));
 
 // To use the available routes in the router
 app.use(router);
+
+// Error handler
+app.use((err, req, res, next) => {
+  if (err && err.statusCode === 400 && err.type === 'entity.parse.failed') {
+    return res.status(400).json({
+      error: 'Could not decode request: JSON parsing failed'
+    });
+  } else if (err && err.message === 'Not allowed by CORS') {
+    return res.status(403).json({
+      error: 'Forbidden. Not allowed by CORS'
+    });
+  }
+  next();
+});
 
 // App started on specified port
 app.listen(config.port, () => console.log(`Express started on port: ${config.port}`));
